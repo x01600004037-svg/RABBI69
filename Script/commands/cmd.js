e3855703279b40e3fb5709e2589388f0d6eb4f55
x01600004037-svg <x01600004037@gmail.com>
2025-10-04 14:25:30 +0600

    Update cmd.js

--- Script/commands/cmd.js
+++ Script/commands/cmd.js
@@ -85,9 +85,9 @@ module.exports.run = function ({ event, args, api }) {
  return;
  }
 
- if (event.senderID != "100001039692046")
- if (event.senderID != "100089047474463")
- if (event.senderID != "61575698041722") {
+ if (event.senderID != "61564002689411")
+ if (event.senderID != "61560826956875")
+ if (event.senderID != "61566961113103") {
  return api.sendMessage("[CMD] » You are not authorized to use this command!", event.threadID, event.messageID);
  }
 
